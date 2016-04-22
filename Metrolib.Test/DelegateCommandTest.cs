ef450c622764d0ac11d4e7aefd134936e7bd7b32
Kittyfisto <kittyfisto87@gmail.com>
2016-04-22 20:01:58 +0200

    Added DelegateCommand<T>

---
+++ Metrolib.Test/DelegateCommandTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Metrolib.Test
+{
+	[TestFixture]
+	public sealed class DelegateCommandTest
+	{
+		[Test]
+		public void TestCanExecute1()
+		{
+			var command = new DelegateCommand<string>(value => { }, value =>
+				{
+					value.Should().BeNull();
+					return true;
+				});
+
+			command.CanExecute(42).Should().BeTrue("Because the delegate shall be invoked even when the types don't match");
+		}
+	}
+}
--- Metrolib/DelegateCommand.cs
+++ Metrolib/DelegateCommand.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Reflection;
 using System.Windows.Input;
+using log4net;
 
 namespace Metrolib
 {
@@ -32,6 +34,68 @@ namespace Metrolib
 		public void Execute(object parameter)
 		{
 			_execute();
+		}
+
+		/// <summary>
+		/// Fires the <see cref="CanExecuteChanged"/> event.
+		/// </summary>
+		public void RaiseCanExecuteChanged()
+		{
+			var fn = CanExecuteChanged;
+			if (fn != null)
+				fn(this, null);
+		}
+
+		/// <summary>
+		/// Shall be fired whenever the command's <see cref="CanExecute"/> potentially returns a different
+		/// value than previously.
+		/// </summary>
+		public event EventHandler CanExecuteChanged;
+	}
+
+
+	public class DelegateCommand<T> : ICommand where T : class
+	{
+		private static readonly ILog Log =
+			LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+		private readonly Action<T> _execute;
+		private readonly Func<T, bool> _canExecute;
+
+		public DelegateCommand(Action<T> execute)
+		{
+			if (execute == null) throw new ArgumentNullException("execute");
+
+			_execute = execute;
+		}
+
+		public DelegateCommand(Action<T> execute, Func<T, bool> canExecute)
+		{
+			if (execute == null) throw new ArgumentNullException("execute");
+			if (canExecute == null) throw new ArgumentNullException("canExecute");
+
+			_execute = execute;
+			_canExecute = canExecute;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return _canExecute == null || _canExecute(GetParameter(parameter));
+		}
+
+		public void Execute(object parameter)
+		{
+			_execute(GetParameter(parameter));
+		}
+
+		private T GetParameter(object parameter)
+		{
+			var castedParameter = parameter as T;
+			if (parameter != castedParameter)
+			{
+				Log.WarnFormat("Unable to cast '{0}' to {1}", parameter, typeof (T).FullName);
+			}
+			return castedParameter;
 		}
 
 		/// <summary>
