11cd3a7a2232ea80e0dea066ea339ba3e314cc2e
Kittyfisto <kittyfisto87@gmail.com>
2016-09-21 00:13:09 +0200

    Added InvertBoolConverter

---
+++ Metrolib.Test/InvertBoolConverterTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Metrolib.Converters;
+using NUnit.Framework;
+
+namespace Metrolib.Test
+{
+	[TestFixture]
+	public sealed class InvertBoolConverterTest
+	{
+		[Test]
+		public void TestConvert1()
+		{
+			var converter = new InvertBoolConverter();
+			converter.Convert(true, typeof(bool), null, null).Should().Be(false);
+			converter.Convert(false, typeof(bool), null, null).Should().Be(true);
+		}
+
+		[Test]
+		public void TestConvert2()
+		{
+			var converter = new InvertBoolConverter();
+			converter.Convert(42, typeof (bool), null, null).Should().BeNull();
+			converter.Convert("false", typeof(bool), null, null).Should().BeNull();
+			converter.Convert("true", typeof(bool), null, null).Should().BeNull();
+			converter.Convert("clondyke bar", typeof(bool), null, null).Should().BeNull();
+		}
+	}
+}
---
+++ Metrolib/Converters/InvertBoolConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Metrolib.Converters
+{
+	/// <summary>
+	///     Inverts the given boolean value.
+	///     Returns null if no boolean value was given.
+	/// </summary>
+	public sealed class InvertBoolConverter
+		: IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is bool))
+				return null;
+
+			return !((bool) value);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
