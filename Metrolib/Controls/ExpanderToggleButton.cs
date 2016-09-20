33ecc3d2f1cfecb14427a99bd0fefc164c3c4720
Kittyfisto <kittyfisto87@gmail.com>
2016-09-20 23:37:51 +0200

    Added VisibilityToggleButton
    Added button to allow toggling of visibility

--- Metrolib/Controls/ExpanderToggleButton.cs
+++ Metrolib/Controls/ExpanderToggleButton.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Controls.Primitives;
 
 namespace Metrolib.Controls
 {
@@ -7,29 +6,12 @@ namespace Metrolib.Controls
 	///     A toggle button that is typically used to expand/contract an area of the UI.
 	/// </summary>
 	public class ExpanderToggleButton
-		: ToggleButton
+		: ExtToggleButton
 	{
-		public static readonly DependencyProperty IsInvertedProperty =
-			DependencyProperty.Register("IsInverted", typeof (bool), typeof (ExpanderToggleButton),
-			                            new PropertyMetadata(false));
-
 		static ExpanderToggleButton()
 		{
 			DefaultStyleKeyProperty.OverrideMetadata(typeof (ExpanderToggleButton),
 			                                         new FrameworkPropertyMetadata(typeof (ExpanderToggleButton)));
-		}
-
-		/// <summary>
-		///     Whether or not the colors of this toggle button are inverted.
-		///     Set to false by default.
-		/// </summary>
-		/// <remarks>
-		///     When inverted, the chevron is drawn in white instead of black.
-		/// </remarks>
-		public bool IsInverted
-		{
-			get { return (bool) GetValue(IsInvertedProperty); }
-			set { SetValue(IsInvertedProperty, value); }
 		}
 	}
 }
---
+++ Metrolib/Controls/VisibilityToggleButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Metrolib.Controls
+{
+	/// <summary>
+	///     A toggle button that allows the user to control whether a filter is being used to include or exclude items.
+	/// </summary>
+	public class VisibilityToggleButton
+		: ExtToggleButton
+	{
+		static VisibilityToggleButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof (VisibilityToggleButton),
+			                                         new FrameworkPropertyMetadata(typeof (VisibilityToggleButton)));
+		}
+	}
+}
