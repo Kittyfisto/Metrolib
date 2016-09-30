56abba0519cb919a0baadd523230cef3780f4bc3
Kittyfisto <kittyfisto87@gmail.com>
2016-10-01 01:52:17 +0200

    Fixed ExpanderToggleButton Style
    - ExpanderToggleButton doesn't need to inherit from ToggleButtonBase
    because it doesn't have much in common with other toggle buttons, as far
    as colors are concerned

--- Metrolib/Controls/ExpanderToggleButton.cs
+++ Metrolib/Controls/ExpanderToggleButton.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
 
 namespace Metrolib.Controls
 {
@@ -6,12 +8,25 @@ namespace Metrolib.Controls
 	///     A toggle button that is typically used to expand/contract an area of the UI.
 	/// </summary>
 	public class ExpanderToggleButton
-		: FlatToggleButton
+		: ToggleButton
 	{
+		public static readonly DependencyProperty InvertedForegroundProperty =
+			DependencyProperty.Register("InvertedForeground", typeof (Brush), typeof (ExpanderToggleButton),
+			                            new PropertyMetadata(default(Brush)));
+
 		static ExpanderToggleButton()
 		{
 			DefaultStyleKeyProperty.OverrideMetadata(typeof (ExpanderToggleButton),
 			                                         new FrameworkPropertyMetadata(typeof (ExpanderToggleButton)));
+		}
+
+		/// <summary>
+		///     The foreground color, used when <see cref="Properties.IsInvertedProperty" /> is set to true.
+		/// </summary>
+		public Brush InvertedForeground
+		{
+			get { return (Brush) GetValue(InvertedForegroundProperty); }
+			set { SetValue(InvertedForegroundProperty, value); }
 		}
 	}
 }
