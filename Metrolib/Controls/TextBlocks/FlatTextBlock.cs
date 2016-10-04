292dbee95850e0f76424901c95c4cef8394d4955
Kittyfisto <kittyfisto87@gmail.com>
2016-10-04 15:03:51 +0200

    Removed unnecessary control

--- Metrolib/Controls/TextBlocks/FlatTextBlock.cs
+++ Metrolib/Controls/TextBlocks/FlatTextBlock.cs
@@ -1,7 +1,11 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+
+// ReSharper disable CheckNamespace
 
 namespace Metrolib.Controls
+// ReSharper restore CheckNamespace
 {
 	/// <summary>
 	///     A "flat" <see cref="TextBlock" /> that supports the <see cref="Properties.IsInvertedProperty" />.
@@ -9,9 +13,20 @@ namespace Metrolib.Controls
 	public class FlatTextBlock
 		: TextBlock
 	{
+		public static readonly DependencyProperty InvertedForegroundProperty =
+			DependencyProperty.Register("InvertedForeground", typeof (Brush), typeof (FlatTextBlock),
+			                            new PropertyMetadata(default(Brush)));
+
 		static FlatTextBlock()
 		{
-			DefaultStyleKeyProperty.OverrideMetadata(typeof(FlatTextBlock), new FrameworkPropertyMetadata(typeof(FlatTextBlock)));
+			DefaultStyleKeyProperty.OverrideMetadata(typeof (FlatTextBlock),
+			                                         new FrameworkPropertyMetadata(typeof (FlatTextBlock)));
+		}
+
+		public Brush InvertedForeground
+		{
+			get { return (Brush) GetValue(InvertedForegroundProperty); }
+			set { SetValue(InvertedForegroundProperty, value); }
 		}
 	}
 }
