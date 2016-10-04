d23031890322b8336974bb8030c0d88a74905f23
Kittyfisto <kittyfisto87@gmail.com>
2016-10-04 20:24:21 +0200

    Added FlatScrollBar and FlatScrollViewer

---
+++ Metrolib/Controls/Scroll/FlatScrollBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+// ReSharper disable CheckNamespace
+namespace Metrolib.Controls
+// ReSharper restore CheckNamespace
+{
+	/// <summary>
+	/// A "flat" scrollbar that features a single bar only (no arrows, border, etc...)
+	/// </summary>
+	public class FlatScrollBar
+		: ScrollBar
+	{
+		static FlatScrollBar()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(FlatScrollBar), new FrameworkPropertyMetadata(typeof(FlatScrollBar)));
+		}
+	}
+}
---
+++ Metrolib/Controls/Scroll/FlatScrollViewer.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+// ReSharper disable CheckNamespace
+namespace Metrolib.Controls
+// ReSharper restore CheckNamespace
+{
+	/// <summary>
+	/// A scroll viewer offering a "flat" view:
+	/// - The scrollbar is situated on top of the client content instead of besides it
+	/// </summary>
+	public class FlatScrollViewer
+		: ScrollViewer
+	{
+		static FlatScrollViewer()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(FlatScrollViewer), new FrameworkPropertyMetadata(typeof(FlatScrollViewer)));
+		}
+	}
+}
