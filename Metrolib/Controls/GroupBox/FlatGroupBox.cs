74f37d9c25df9d2dda1773da29a1a58eed7b8b8c
Kittyfisto <kittyfisto87@gmail.com>
2016-10-11 22:24:40 +0200

    Added FlatGroupBox

---
+++ Metrolib/Controls/GroupBox/FlatGroupBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+// ReSharper disable CheckNamespace
+namespace Metrolib.Controls
+// ReSharper restore CheckNamespace
+{
+	/// <summary>
+	/// A group box in a flat style:
+	/// - No border
+	/// - Header has increased font size
+	/// </summary>
+	public class FlatGroupBox
+		: GroupBox
+	{
+		static FlatGroupBox()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(FlatGroupBox), new FrameworkPropertyMetadata(typeof(FlatGroupBox)));
+		}
+	}
+}
