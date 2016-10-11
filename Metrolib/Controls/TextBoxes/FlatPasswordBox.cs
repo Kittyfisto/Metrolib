159566f360ed27e7ec42b399f7f2e99b72d1e8bd
Kittyfisto <kittyfisto87@gmail.com>
2016-10-12 01:06:24 +0200

    Added FlatPasswordBox

---
+++ Metrolib/Controls/TextBoxes/FlatPasswordBox.cs
@@ -0,0 +1,107 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+// ReSharper disable CheckNamespace
+
+namespace Metrolib.Controls
+// ReSharper restore CheckNamespace
+{
+	/// <summary>
+	/// </summary>
+	[TemplatePart(Name = "PART_PasswordBox", Type = typeof (PasswordBox))]
+	[TemplatePart(Name = "PART_Watermark", Type = typeof (TextBlock))]
+	public class FlatPasswordBox
+		: Control
+	{
+		/// <summary>
+		///     Definition of the <see cref="Watermark" /> property.
+		/// </summary>
+		public static readonly DependencyProperty WatermarkProperty =
+			DependencyProperty.Register("Watermark", typeof (string), typeof (FlatPasswordBox),
+			                            new PropertyMetadata(default(string)));
+
+		private PasswordBox _passwordBox;
+		private TextBlock _watermark;
+
+		static FlatPasswordBox()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof (FlatPasswordBox),
+			                                         new FrameworkPropertyMetadata(typeof (FlatPasswordBox)));
+		}
+
+		/// <summary>
+		///     The watermark that is displayed for as long as no password has been entered.
+		/// </summary>
+		public string Watermark
+		{
+			get { return (string) GetValue(WatermarkProperty); }
+			set { SetValue(WatermarkProperty, value); }
+		}
+
+		/// <summary>
+		///     The password that has been entered by the user.
+		/// </summary>
+		/// <remarks>
+		///     Can not be bound to.
+		/// </remarks>
+		public string Password
+		{
+			get
+			{
+				return _passwordBox != null
+					       ? _passwordBox.Password
+					       : null;
+			}
+			set
+			{
+				if (value == Password)
+					return;
+
+				if (_passwordBox != null)
+				{
+					_passwordBox.Password = value;
+					UpdateWatermarkVisibility();
+
+					var fn = PasswordChanged;
+					if (fn != null)
+						fn(this, new RoutedEventArgs());
+				}
+			}
+		}
+
+		/// <summary>
+		///     Is fired whenever the <see cref="Password" /> changes.
+		/// </summary>
+		public event RoutedEventHandler PasswordChanged;
+
+		public override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			if (_passwordBox != null)
+			{
+				_passwordBox.PasswordChanged -= PasswordBoxOnPasswordChanged;
+			}
+
+			_passwordBox = (PasswordBox) GetTemplateChild("PART_PasswordBox");
+			if (_passwordBox != null)
+			{
+				_passwordBox.PasswordChanged += PasswordBoxOnPasswordChanged;
+			}
+
+			_watermark = (TextBlock) GetTemplateChild("PART_Watermark");
+		}
+
+		private void PasswordBoxOnPasswordChanged(object sender, RoutedEventArgs routedEventArgs)
+		{
+			UpdateWatermarkVisibility();
+		}
+
+		private void UpdateWatermarkVisibility()
+		{
+			_watermark.Visibility = string.IsNullOrEmpty(Password)
+				                        ? Visibility.Visible
+				                        : Visibility.Hidden;
+		}
+	}
+}
