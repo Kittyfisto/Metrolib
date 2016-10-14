574a49b11286c25a2015cf1ce5eeef0e943006ef
Kittyfisto <kittyfisto87@gmail.com>
2016-10-14 11:55:32 +0200

    FlatContextMenu can host MenuItemContentControl

--- Metrolib/Controls/ContextMenu/FlatContextMenu.cs
+++ Metrolib/Controls/ContextMenu/FlatContextMenu.cs
@@ -12,7 +12,14 @@ namespace Metrolib.Controls
 {
 	/// <summary>
 	///     A context menu that is styled in the typical flat look.
+	///     By default, all content is hosted inside <see cref="FlatMenuItem" />s which behave just like regular
+	///     <see cref="MenuItem" />s.
 	/// </summary>
+	/// <remarks>
+	///     In addition, content can be hosted inside a <see cref="MenuItemContentControl" /> which
+	///     does NOT behave like a <see cref="MenuItem" /> at all. It allows the placement of any custom
+	///     content inside a ContextMenu besides <see cref="MenuItem" />s.
+	/// </remarks>
 	public class FlatContextMenu
 		: ContextMenu
 	{
@@ -77,13 +84,15 @@ namespace Metrolib.Controls
 		}
 
 		/// <summary>
-		///     Determines whether an object is a Metrolib.Controls.FlatMenuItem.
+		///     Determines whether an object is a <see cref="Metrolib.Controls.FlatMenuItem" /> or
+		///     a <see cref="Metrolib.Controls.MenuItemContentControl" />.
 		/// </summary>
 		/// <param name="item"></param>
 		/// <returns></returns>
 		protected override bool IsItemItsOwnContainerOverride(object item)
 		{
-			return item is FlatMenuItem;
+			return item is FlatMenuItem ||
+			       item is MenuItemContentControl;
 		}
 
 		private void TimerOnTick(object sender, EventArgs eventArgs)
---
+++ Metrolib/Controls/ContextMenu/MenuItemContentControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+// ReSharper disable CheckNamespace
+namespace Metrolib.Controls
+// ReSharper restore CheckNamespace
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class MenuItemContentControl
+		: ContentControl
+	{
+		static MenuItemContentControl()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof (MenuItemContentControl),
+			                                         new FrameworkPropertyMetadata(typeof (MenuItemContentControl)));
+		}
+	}
+}
