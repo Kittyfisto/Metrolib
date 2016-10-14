5caf1e99dc34706a17a8750bf9e145fb9e56fd54
Kittyfisto <kittyfisto87@gmail.com>
2016-10-14 15:43:45 +0200

    Added BulletinsItemsControl
    - Presents bulletin lists

---
+++ Metrolib/Controls/BulletinItemsControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Metrolib.Controls
+{
+	/// <summary>
+	///     An items control that presents each item with a bulletin point next to it.
+	/// </summary>
+	public class BulletinItemsControl
+		: Control
+	{
+		/// <summary>
+		///     Definition of the <see cref="ItemsSource" /> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty ItemsSourceProperty =
+			DependencyProperty.Register("ItemsSource", typeof (IEnumerable), typeof (BulletinItemsControl),
+			                            new PropertyMetadata(default(IEnumerable)));
+
+		/// <summary>
+		///     Definition of the <see cref="BulletinMargin" /> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty BulletinMarginProperty =
+			DependencyProperty.Register("BulletinMargin", typeof (Thickness), typeof (BulletinItemsControl),
+			                            new PropertyMetadata(default(Thickness)));
+
+		static BulletinItemsControl()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof (BulletinItemsControl),
+			                                         new FrameworkPropertyMetadata(typeof (BulletinItemsControl)));
+		}
+
+		/// <summary>
+		///     The margin of the bulletin that's left to the actual content.
+		/// </summary>
+		public Thickness BulletinMargin
+		{
+			get { return (Thickness) GetValue(BulletinMarginProperty); }
+			set { SetValue(BulletinMarginProperty, value); }
+		}
+
+		/// <summary>
+		///     The list of items to be presented.
+		/// </summary>
+		public IEnumerable ItemsSource
+		{
+			get { return (IEnumerable) GetValue(ItemsSourceProperty); }
+			set { SetValue(ItemsSourceProperty, value); }
+		}
+	}
+}
