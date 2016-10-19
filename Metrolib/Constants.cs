3da694d25d4f16a7a0b9c101354db3591349c1e7
Kittyfisto <kittyfisto87@gmail.com>
2016-10-19 21:22:44 +0200

    Added HyperlinkRun to allow placing hyperlinks in larger texts

---
+++ Metrolib/Constants.cs
@@ -0,0 +1,86 @@
+﻿using System.Windows.Media;
+
+namespace Metrolib
+{
+	/// <summary>
+	/// </summary>
+	public static class Constants
+	{
+		#region Colors
+
+		/// <summary>
+		/// </summary>
+		public static readonly Color ForegroundColor;
+
+		/// <summary>
+		/// </summary>
+		public static readonly Color ForegroundColorAccent;
+
+		/// <summary>
+		/// </summary>
+		public static readonly Color ForegroundColorHovered;
+
+		/// <summary>
+		/// </summary>
+		public static readonly Color ForegroundColorPressed;
+
+		/// <summary>
+		/// </summary>
+		public static readonly Color ForegroundColorDisabled;
+
+		#endregion
+
+		#region Brushes
+
+		/// <summary>
+		/// </summary>
+		public static readonly SolidColorBrush ForegroundBrush;
+
+		/// <summary>
+		/// </summary>
+		public static readonly SolidColorBrush ForegroundBrushAccent;
+
+		/// <summary>
+		/// </summary>
+		public static readonly SolidColorBrush ForegroundBrushHovered;
+
+		/// <summary>
+		/// </summary>
+		public static readonly SolidColorBrush ForegroundBrushPressed;
+
+		/// <summary>
+		/// </summary>
+		public static readonly SolidColorBrush ForegroundBrushDisabled;
+
+		#endregion
+
+		static Constants()
+		{
+			ForegroundColor = Color("#333333");
+			ForegroundColorAccent = Color("#3998D6");
+			ForegroundColorHovered = Color("#2061A1");
+			ForegroundColorPressed = Color("#0B4680");
+			ForegroundColorDisabled = Color("#C9DAEB");
+
+			ForegroundBrush = new SolidColorBrush(ForegroundColor);
+			ForegroundBrush.Freeze();
+
+			ForegroundBrushAccent = new SolidColorBrush(ForegroundColorAccent);
+			ForegroundBrushAccent.Freeze();
+
+			ForegroundBrushHovered = new SolidColorBrush(ForegroundColorHovered);
+			ForegroundBrushHovered.Freeze();
+
+			ForegroundBrushPressed = new SolidColorBrush(ForegroundColorPressed);
+			ForegroundBrushPressed.Freeze();
+
+			ForegroundBrushDisabled = new SolidColorBrush(ForegroundColorDisabled);
+			ForegroundBrushDisabled.Freeze();
+		}
+
+		private static Color Color(string color)
+		{
+			return (Color) ColorConverter.ConvertFromString(color);
+		}
+	}
+}
---
+++ Metrolib/Controls/TextBlocks/HyperlinkRun.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using log4net;
+
+namespace Metrolib.Controls.TextBlocks
+{
+	/// <summary>
+	///     A run that is styled to look like a typical hyperlink.
+	///     Text is blue, the mouse changes its cursor on mouse over and the text changes color when
+	///     hovered/pressed.
+	/// </summary>
+	/// <remarks>
+	///     Is used by the <see cref="OpenInNewHyperlink" /> control which is suitable for most cases.
+	///     This run should be used when the hyperlink is required to be part of a larger text (just place
+	///     is next to any other run).
+	/// </remarks>
+	public class HyperlinkRun
+		: Run
+	{
+		private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+		/// <summary>
+		///     Definition of the <see cref="NavigateUri" /> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty NavigateUriProperty =
+			DependencyProperty.Register("NavigateUri", typeof (Uri), typeof (HyperlinkRun), new PropertyMetadata(default(Uri)));
+
+		/// <summary>
+		///     Definition of the <see cref="ProcessName" /> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty ProcessNameProperty =
+			DependencyProperty.Register("ProcessName", typeof (string), typeof (HyperlinkRun),
+			                            new PropertyMetadata(default(string)));
+
+		/// <summary>
+		///     Definition of the <see cref="ProcessStartArguments" /> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty ProcessStartArgumentsProperty =
+			DependencyProperty.Register("ProcessStartArguments", typeof (string), typeof (HyperlinkRun),
+			                            new PropertyMetadata(default(string)));
+
+		private static readonly DependencyPropertyKey IsPressedPropertyKey
+			= DependencyProperty.RegisterReadOnly("IsPressed", typeof (bool), typeof (HyperlinkRun),
+			                                      new FrameworkPropertyMetadata(false,
+			                                                                    FrameworkPropertyMetadataOptions.None,
+			                                                                    OnIsPressedChanged));
+
+		/// <summary>
+		///     Definition of the <see cref="IsPressed" /> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty IsPressedProperty
+			= IsPressedPropertyKey.DependencyProperty;
+
+		/// <summary>
+		/// Initializes this run.
+		/// </summary>
+		public HyperlinkRun()
+		{
+			MouseLeftButtonUp += OnMouseLeftButtonUp;
+			MouseLeftButtonDown += OnMouseLeftButtonDown;
+			TouchDown += OnTouchDown;
+			TouchUp += OnTouchUp;
+			MouseEnter += OnMouseEnter;
+			MouseLeave += OnMouseLeave;
+			IsEnabledChanged += OnIsEnabledChanged;
+
+			UpdateForeground();
+		}
+
+		/// <summary>
+		///     The URI the browser should navigate to.
+		/// </summary>
+		public Uri NavigateUri
+		{
+			get { return (Uri) GetValue(NavigateUriProperty); }
+			set { SetValue(NavigateUriProperty, value); }
+		}
+
+		/// <summary>
+		///     The process that shall be started via <see cref="Process.Start()" />.
+		/// </summary>
+		public string ProcessName
+		{
+			get { return (string) GetValue(ProcessNameProperty); }
+			set { SetValue(ProcessNameProperty, value); }
+		}
+
+		/// <summary>
+		///     The arguments that shall be passed to <see cref="Process.Start()" />.
+		/// </summary>
+		public string ProcessStartArguments
+		{
+			get { return (string) GetValue(ProcessStartArgumentsProperty); }
+			set { SetValue(ProcessStartArgumentsProperty, value); }
+		}
+
+		/// <summary>
+		///     Whether or not this control is currently being pressed by the left mouse button
+		///     or a touch gesture.
+		/// </summary>
+		public bool IsPressed
+		{
+			get { return (bool) GetValue(IsPressedProperty); }
+			protected set { SetValue(IsPressedPropertyKey, value); }
+		}
+
+		private static void OnIsPressedChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+		{
+			((HyperlinkRun) dependencyObject).OnIsPressedChanged((bool) args.NewValue);
+		}
+
+		private void OnIsPressedChanged(bool isPressed)
+		{
+			UpdateForeground();
+		}
+
+		private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			IsPressed = true;
+			CaptureMouse();
+		}
+
+		private void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			if (IsMouseCaptured)
+			{
+				ReleaseMouseCapture();
+
+				Navigate();
+
+				e.Handled = true;
+			}
+		}
+
+		private void OnTouchDown(object sender, TouchEventArgs e)
+		{
+			IsPressed = true;
+		}
+
+		private void OnTouchUp(object sender, TouchEventArgs touchEventArgs)
+		{
+			IsPressed = false;
+			Navigate();
+		}
+
+		private void OnMouseEnter(object sender, MouseEventArgs e)
+		{
+			Cursor = Cursors.Hand;
+			UpdateForeground();
+		}
+
+		private void OnMouseLeave(object sender, MouseEventArgs e)
+		{
+			Cursor = null;
+			UpdateForeground();
+		}
+
+		private void OnIsEnabledChanged(object sender, DependencyPropertyChangedEventArgs args)
+		{
+			UpdateForeground();
+		}
+
+		private void Navigate()
+		{
+			Uri uri = NavigateUri;
+			string processName = ProcessName;
+			if (uri != null)
+			{
+				var args = new ProcessStartInfo(NavigateUri.ToString());
+				Start(args);
+			}
+			else if (processName != null)
+			{
+				var args = new ProcessStartInfo(processName, ProcessStartArguments);
+				Start(args);
+			}
+			else
+			{
+				Log.WarnFormat("Neither NavigateUri, nor ProcessName given, can't start process!");
+			}
+		}
+
+		private void Start(ProcessStartInfo info)
+		{
+			try
+			{
+				Process.Start(info);
+			}
+			catch (Exception e)
+			{
+				Log.ErrorFormat("Cauhgt unexpected exception: {0}", e);
+			}
+		}
+
+		private void UpdateForeground()
+		{
+			if (!IsEnabled)
+			{
+				Foreground = Constants.ForegroundBrushDisabled;
+			}
+			else if (IsPressed)
+			{
+				Foreground = Constants.ForegroundBrushPressed;
+			}
+			else if (IsMouseOver)
+			{
+				Foreground = Constants.ForegroundBrushHovered;
+			}
+			else
+			{
+				Foreground = Constants.ForegroundBrushAccent;
+			}
+		}
+	}
+}
