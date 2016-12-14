250a9e57b192a1ef6c855a8379f0cbf6a48e1b43
Kittyfisto <kittyfisto87@gmail.com>
2016-12-14 21:04:21 +0100

    Added documentation

--- Metrolib/Converters/TimeSpanConverter.cs
+++ Metrolib/Converters/TimeSpanConverter.cs
@@ -69,6 +69,14 @@ namespace Metrolib.Converters
 			return Format(age, oneMillisecond, "millisecond");
 		}
 
+		/// <summary>
+		///     Converts a value.
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="targetType"></param>
+		/// <param name="parameter"></param>
+		/// <param name="culture"></param>
+		/// <returns></returns>
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
 		{
 			return null;
