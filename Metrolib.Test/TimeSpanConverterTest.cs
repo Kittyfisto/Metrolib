6df806c7af43b6d48164b0a857d9e71b21ca4588
Kittyfisto <kittyfisto87@gmail.com>
2016-12-14 20:43:02 +0100

    Fixed #10
    - TimeSpanConverter is accessible
    - Added unit tests
    - Increased range of allowed values

---
+++ Metrolib.Test/TimeSpanConverterTest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Globalization;
+using FluentAssertions;
+using Metrolib.Converters;
+using NUnit.Framework;
+
+namespace Metrolib.Test
+{
+	[TestFixture]
+	public sealed class TimeSpanConverterTest
+	{
+		[SetUp]
+		public void Setup()
+		{
+			_converter = new TimeSpanConverter();
+		}
+
+		private TimeSpanConverter _converter;
+
+		[Test]
+		public void TestConvert1()
+		{
+			_converter.Convert(TimeSpan.Zero, typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("0 milliseconds");
+			_converter.Convert(TimeSpan.FromMilliseconds(1), typeof (string), null, CultureInfo.CurrentUICulture).Should().Be("1 millisecond");
+			_converter.Convert(TimeSpan.FromMilliseconds(2), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("2 milliseconds");
+			_converter.Convert(TimeSpan.FromMilliseconds(999), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("999 milliseconds");
+		}
+
+		[Test]
+		public void TestConvert2()
+		{
+			_converter.Convert(TimeSpan.FromSeconds(1), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("1 second");
+			_converter.Convert(TimeSpan.FromSeconds(2), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("2 seconds");
+			_converter.Convert(TimeSpan.FromSeconds(59), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("59 seconds");
+		}
+
+		[Test]
+		public void TestConvert3()
+		{
+			_converter.Convert(TimeSpan.FromMinutes(1), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("1 minute");
+			_converter.Convert(TimeSpan.FromMinutes(2), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("2 minutes");
+			_converter.Convert(TimeSpan.FromMinutes(59), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("59 minutes");
+		}
+
+		[Test]
+		public void TestConvert4()
+		{
+			_converter.Convert(TimeSpan.FromHours(1), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("1 hour");
+			_converter.Convert(TimeSpan.FromHours(2), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("2 hours");
+			_converter.Convert(TimeSpan.FromHours(23), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("23 hours");
+		}
+
+		[Test]
+		public void TestConvert5()
+		{
+			_converter.Convert(TimeSpan.FromDays(1), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("1 day");
+			_converter.Convert(TimeSpan.FromDays(2), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("2 days");
+			_converter.Convert(TimeSpan.FromDays(6), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("6 days");
+		}
+
+		[Test]
+		public void TestConvert6()
+		{
+			_converter.Convert(TimeSpan.FromDays(7), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("1 week");
+			_converter.Convert(TimeSpan.FromDays(14), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("2 weeks");
+			_converter.Convert(TimeSpan.FromDays(29), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("4 weeks");
+		}
+
+		[Test]
+		public void TestConvert7()
+		{
+			_converter.Convert(TimeSpan.FromDays(30.5), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("1 month");
+			_converter.Convert(TimeSpan.FromDays(61), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("2 months");
+			_converter.Convert(TimeSpan.FromDays(364), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("11 months");
+		}
+
+		[Test]
+		public void TestConvert8()
+		{
+			_converter.Convert(TimeSpan.FromDays(365.25), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("1 year");
+			_converter.Convert(TimeSpan.FromDays(800), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("2 years");
+			_converter.Convert(TimeSpan.FromDays(3649), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("9 years");
+		}
+
+		[Test]
+		public void TestConvert9()
+		{
+			_converter.Convert(TimeSpan.FromDays(3652.5), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("1 decade");
+			_converter.Convert(TimeSpan.FromDays(7305), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("2 decades");
+			_converter.Convert(TimeSpan.FromDays(36524), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("9 decades");
+		}
+
+		[Test]
+		public void TestConvert10()
+		{
+			_converter.Convert(TimeSpan.FromDays(36525), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("1 century");
+			_converter.Convert(TimeSpan.FromDays(73050), typeof(string), null, CultureInfo.CurrentUICulture).Should().Be("2 centuries");
+		}
+
+		[Test]
+		public void TestConvert11()
+		{
+			new Action(() => _converter.Convert(null, typeof(string), null, CultureInfo.InvariantCulture)).ShouldNotThrow();
+			new Action(() => _converter.Convert(42, typeof(string), null, CultureInfo.InvariantCulture)).ShouldNotThrow();
+			new Action(() => _converter.Convert("foobar", typeof(string), null, CultureInfo.InvariantCulture)).ShouldNotThrow();
+		}
+	}
+}
--- Metrolib/Converters/TimeSpanConverter.cs
+++ Metrolib/Converters/TimeSpanConverter.cs
@@ -7,34 +7,66 @@ using System.Windows.Data;
 namespace Metrolib.Converters
 // ReSharper restore CheckNamespace
 {
-	internal sealed class TimeSpanConverter
+	/// <summary>
+	///     A value converter that converts <see cref="TimeSpan" /> values into a written representation that rounds down
+	///     to the nearest unit:
+	///     - n milisecond(s)
+	///     - n second(s)
+	///     - n minute(s)
+	///     - n hour(s)
+	///     - n day(s)
+	///     - n week(s)
+	///     - n year(s)
+	///     - n decade(s)
+	/// </summary>
+	public sealed class TimeSpanConverter
 		: IValueConverter
 	{
+		/// <summary>
+		///     Converts a value.
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="targetType"></param>
+		/// <param name="parameter"></param>
+		/// <param name="culture"></param>
+		/// <returns></returns>
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
 			if (!(value is TimeSpan))
 				return null;
 
 			var age = (TimeSpan) value;
-			TimeSpan oneMonth = TimeSpan.FromDays(30);
+			TimeSpan oneCentury = TimeSpan.FromDays(36525);
+			TimeSpan oneDecade = TimeSpan.FromDays(3652.5);
+			TimeSpan oneYear = TimeSpan.FromDays(365.25);
+			TimeSpan oneMonth = TimeSpan.FromDays(30.5);
 			TimeSpan oneWeek = TimeSpan.FromDays(7);
 			TimeSpan oneDay = TimeSpan.FromDays(1);
 			TimeSpan oneHour = TimeSpan.FromHours(1);
 			TimeSpan oneMinute = TimeSpan.FromMinutes(1);
 			TimeSpan oneSecond = TimeSpan.FromSeconds(1);
+			TimeSpan oneMillisecond = TimeSpan.FromMilliseconds(1);
 
-			if (age > oneMonth)
-				return Format(age, oneMonth, "day");
-			if (age > oneWeek)
+			if (age >= oneCentury)
+				return Format(age, oneCentury, "century", "centuries");
+			if (age >= oneDecade)
+				return Format(age, oneDecade, "decade");
+			if (age >= oneYear)
+				return Format(age, oneYear, "year");
+			if (age >= oneMonth)
+				return Format(age, oneMonth, "month");
+			if (age >= oneWeek)
 				return Format(age, oneWeek, "week");
-			if (age > oneDay)
+			if (age >= oneDay)
 				return Format(age, oneDay, "day");
-			if (age > oneHour)
+			if (age >= oneHour)
 				return Format(age, oneHour, "hour");
-			if (age > oneMinute)
+			if (age >= oneMinute)
 				return Format(age, oneMinute, "minute");
+			if (age >= oneSecond)
+				return Format(age, oneSecond, "second");
 
-			return Format(age, oneSecond, "second");
+			return Format(age, oneMillisecond, "millisecond");
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -44,11 +76,13 @@ namespace Metrolib.Converters
 
 		private object Format(TimeSpan value, TimeSpan divider, string caption)
 		{
-			var number = (int) (value.TotalMilliseconds/divider.TotalMilliseconds);
-			if (number == 1)
-				return string.Format("{0} {1}", number, caption);
+			return Format(value, divider, caption, caption + "s");
+		}
 
-			return string.Format("{0} {1}s", number, caption);
+		private object Format(TimeSpan value, TimeSpan divider, string singular, string plural)
+		{
+			var number = (int) (value.TotalMilliseconds/divider.TotalMilliseconds);
+			return string.Format("{0} {1}", number, number == 1 ? singular : plural);
 		}
 	}
 }
