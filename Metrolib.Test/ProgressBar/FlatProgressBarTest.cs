8483ec91b9f61704daf6ab654739b93bd0810774
Kittyfisto <kittyfisto87@gmail.com>
2016-12-21 19:13:27 +0100

    Added unit test for FlatProgressBar

---
+++ Metrolib.Test/ProgressBar/FlatProgressBarTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Metrolib.Test.ProgressBar
+{
+	[TestFixture]
+	public sealed class FlatProgressBarTest
+	{
+		[Test]
+		[STAThread]
+		public void TestCtor()
+		{
+			var bar = new FlatProgressBar();
+			bar.Minimum.Should().Be(0);
+			bar.Maximum.Should().Be(100);
+			bar.Value.Should().Be(0);
+		}
+	}
+}
