f6cc5164d2114637837469aa95c9734c2e826a17
Kittyfisto <kittyfisto87@gmail.com>
2017-03-28 22:32:51 +0200

    Added more visibility converters

---
+++ Metrolib/Converters/NullToCollapsedConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Metrolib.Converters
+{
+	/// <summary>
+	///     Converts null to <see cref="Visibility.Visible" />, anything else to <see cref="Visibility.Visible" />.
+	/// </summary>
+	public sealed class NullToCollapsedConverter
+		: IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value == null
+				? Visibility.Collapsed
+				: Visibility.Visible;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return Binding.DoNothing;
+		}
+	}
+}
---
+++ Metrolib/Converters/NullToVisibleConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Metrolib.Converters
+{
+	/// <summary>
+	///     Converts null to <see cref="Visibility.Visible" />, anything else to <see cref="Visibility.Collapsed" />.
+	/// </summary>
+	public sealed class NullToVisibleConverter
+		: IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value == null
+				? Visibility.Visible
+				: Visibility.Collapsed;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return Binding.DoNothing;
+		}
+	}
+}
