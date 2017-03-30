2db0706727ef3f4b98b34526f9948a0d2621d84b
Kittyfisto <kittyfisto87@gmail.com>
2017-03-30 20:20:13 +0200

    Introduced Version extension methods

---
+++ Metrolib/VersionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Metrolib
+{
+	/// <summary>
+	///     Provides extension methods for the <see cref="Version" /> class.
+	/// </summary>
+	public static class VersionExtensions
+	{
+		/// <summary>
+		///     Produces a humna readable string of this version.
+		/// </summary>
+		/// <param name="version"></param>
+		/// <returns></returns>
+		public static string Format(this Version version)
+		{
+			var value = version.ToString(3);
+			return value;
+		}
+	}
+}
