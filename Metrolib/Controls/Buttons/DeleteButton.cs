6e7450e58cbbe598e8603201e99660a006023d41
Kittyfisto <kittyfisto87@gmail.com>
2017-03-31 23:15:17 +0200

    Introduced DeleteButton

---
+++ Metrolib/Controls/Buttons/DeleteButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Metrolib.Controls
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class DeleteButton
+		 : FlatButton
+	{
+		static DeleteButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(DeleteButton),
+				new FrameworkPropertyMetadata(typeof(DeleteButton)));
+		}
+	}
+}
