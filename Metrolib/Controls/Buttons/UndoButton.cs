e1dfee1da81ada1cd15ae870e3966ae50e7888fb
Kittyfisto <kittyfisto87@gmail.com>
2017-04-15 13:31:18 +0200

    Added UndoButton

---
+++ Metrolib/Controls/Buttons/UndoButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+// ReSharper disable once CheckNamespace
+namespace Metrolib.Controls
+{
+	/// <summary>
+	///     A button to undo something, for example a textbox change.
+	/// </summary>
+	public class UndoButton
+		: FlatButton
+	{
+		static UndoButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(UndoButton),
+				new FrameworkPropertyMetadata(typeof(UndoButton)));
+		}
+	}
+}
