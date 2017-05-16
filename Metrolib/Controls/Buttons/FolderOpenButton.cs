e766114f2998a336c0249ef06b9950ee53fb0bac
Kittyfisto <kittyfisto87@gmail.com>
2017-05-16 21:15:35 +0200

    Introduced FolderOpenButton

---
+++ Metrolib/Controls/Buttons/FolderOpenButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+// ReSharper disable once CheckNamespace
+namespace Metrolib.Controls
+{
+	/// <summary>
+	///     A button that should spawn a folder open dialog or a new explorer folder.
+	/// </summary>
+	public class FolderOpenButton : FlatButton
+	{
+		static FolderOpenButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(FolderOpenButton),
+				new FrameworkPropertyMetadata(typeof(FolderOpenButton)));
+		}
+	}
+}
