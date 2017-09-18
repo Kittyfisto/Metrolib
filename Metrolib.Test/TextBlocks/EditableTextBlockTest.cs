eaa81ca0043e2973ae68dee69dbf2c243e29a4a9
Kittyfisto <kittyfisto87@gmail.com>
2017-09-18 22:25:08 +0200

    Added EditableTextBlock control
    Added control that displays a text which may optionally be edited, when double clicked.

---
+++ Metrolib.Test/TextBlocks/EditableTextBlockTest.cs
@@ -0,0 +1,129 @@
+﻿using System.Threading;
+using System.Windows.Input;
+using FluentAssertions;
+using Metrolib.Controls;
+using NUnit.Framework;
+using WpfUnit;
+
+namespace Metrolib.Test.TextBlocks
+{
+	[TestFixture]
+	[RequiresThread(ApartmentState.STA)]
+	public sealed class EditableTextBlockTest
+	{
+		private EditableTextBlock _control;
+		private TestMouse _mouse;
+		private TestKeyboard _keyboard;
+
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			_mouse = new TestMouse();
+			_keyboard = new TestKeyboard();
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+			_control = new EditableTextBlock
+			{
+				Style = StyleHelper.Load<EditableTextBlock>()
+			};
+			_control.ApplyTemplate();
+		}
+
+		[Test]
+		[Ignore("https://github.com/Kittyfisto/WpfUnit/issues/1")]
+		public void TestEnableEditing()
+		{
+			_control.Text = "Hello, World!";
+
+			_control.IsEditing.Should().BeFalse();
+			_control.TextBlock.IsVisible.Should().BeTrue();
+			_control.TextBox.IsVisible.Should().BeFalse();
+			_control.TextBox.IsFocused.Should().BeFalse();
+
+			_mouse.LeftClick(_control.TextBlock);
+			_mouse.LeftClick(_control.TextBlock);
+
+			_control.IsEditing.Should().BeTrue();
+			// _control.Dispatcher.ExecuteAllPendingEvents();
+			_control.TextBlock.IsVisible.Should().BeFalse();
+			_control.TextBox.IsVisible.Should().BeTrue("because enabling editing shall show the textbox");
+			_control.TextBox.IsFocused.Should().BeTrue();
+			_control.TextBox.SelectedText.Should().Be("Hello, World!", "because the entire text shall be selected by default");
+		}
+
+		[Test]
+		[Ignore("https://github.com/Kittyfisto/WpfUnit/issues/2")]
+		public void TestDisableEditing1()
+		{
+			_control.IsEditing = true;
+			// _control.Dispatcher.ExecuteAllPendingEvents();
+			_control.TextBox.IsVisible.Should().BeTrue();
+
+			_control.IsEditing = false;
+			_control.TextBox.IsVisible.Should().BeFalse();
+		}
+
+		[Test]
+		public void TestDisableEditing2()
+		{
+			_control.IsEditing = true;
+
+			_keyboard.Click(_control.TextBox, Key.Escape);
+			_control.IsEditing.Should().BeFalse("because pressing escape shall disable editing");
+		}
+
+		[Test]
+		public void TestDisableEditing3()
+		{
+			_control.IsEditing = true;
+
+			_keyboard.Click(_control.TextBox, Key.Enter);
+			_control.IsEditing.Should().BeFalse("because pressing enter shall disable editing");
+		}
+
+		[Test]
+		[Description("Verifies that changes to the textbox are only ever reflected back to the text property when editing has concluded")]
+		public void TestEdit1()
+		{
+			_control.Text = "Hello,";
+			_control.IsEditing = true;
+			_control.TextBox.Text.Should().Be("Hello,");
+			_control.TextBox.Text = "Hello, World!";
+			_control.Text.Should().Be("Hello,", "because we haven't finished editing");
+
+			_control.IsEditing = false;
+			_control.Text.Should().Be("Hello, World!",
+				"because we've finished editing and thus the edited value should've been written to the text property");
+		}
+
+		[Test]
+		[Description("Verifies that changes to the textbox are only ever reflected back to the text property when editing has concluded")]
+		public void TestEdit2()
+		{
+			_control.Text = "Hello,";
+			_control.IsEditing = true;
+			_control.TextBox.Text.Should().Be("Hello,");
+			_control.TextBox.Text = "Hello, World!";
+			_control.Text.Should().Be("Hello,", "because we haven't finished editing");
+
+			_keyboard.Click(_control.TextBox, Key.Enter);
+			_control.Text.Should().Be("Hello, World!",
+				"because we've finished editing and thus the edited value should've been written to the text property");
+		}
+
+		[Test]
+		[Description("Verifies that the Text property remains unchanged when the user cancels editing with escape")]
+		public void TestCancelEditing()
+		{
+			_control.Text = "stuff!";
+			_control.IsEditing = true;
+			_control.TextBox.Text.Should().Be("stuff!");
+			_control.TextBox.Text = "some stuff!";
+			_keyboard.Click(_control.TextBox, Key.Escape);
+			_control.Text.Should().Be("stuff!", "because we cancelled the user input and thus the text value should remain unchanged");
+		}
+	}
+}
---
+++ Metrolib/Controls/TextBlocks/EditableTextBlock.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+// ReSharper disable once CheckNamespace
+namespace Metrolib.Controls
+{
+	/// <summary>
+	///     A control which behaves like a text block, until it's set to be editable.
+	///     When editable, the textblock is replaced by a textbox, which allows the user to
+	///     change the text property.
+	/// </summary>
+	[TemplatePart(Name = PART_TextBlock, Type = typeof(TextBlock))]
+	[TemplatePart(Name = PART_TextBox, Type = typeof(TextBox))]
+	public sealed class EditableTextBlock
+		: Control
+	{
+		/// <summary>
+		///     Name of the <see cref="TextBlock" /> control.
+		/// </summary>
+		public const string PART_TextBlock = "PART_TextBlock";
+
+		/// <summary>
+		///     Text of the <see cref="TextBox" /> control.
+		/// </summary>
+		public const string PART_TextBox = "PART_TextBox";
+
+		/// <summary>
+		///     Definition of the <see cref="IsEditing" /> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty IsEditingProperty = DependencyProperty.Register(
+			"IsEditing", typeof(bool), typeof(EditableTextBlock),
+			new PropertyMetadata(defaultValue: false, propertyChangedCallback: OnIsEditingChanged));
+
+		/// <summary>
+		///     Definition of the <see cref="Text" /> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+			"Text", typeof(string), typeof(EditableTextBlock), new PropertyMetadata(default(string)));
+
+		static EditableTextBlock()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(EditableTextBlock),
+				new FrameworkPropertyMetadata(typeof(EditableTextBlock)));
+		}
+
+		/// <summary>
+		///     For testing.
+		/// </summary>
+		internal TextBlock TextBlock { get; private set; }
+
+		/// <summary>
+		///     For testing.
+		/// </summary>
+		internal TextBox TextBox { get; private set; }
+
+		/// <summary>
+		///     Whether or not the text of this control is being edited.
+		/// </summary>
+		/// <remarks>
+		///     The user can enter this state by double clicking / tapping this control.
+		///     The user can leave this state by removing focus from this control (for example by pressing escape/tab).
+		/// </remarks>
+		/// <remarks>
+		///     Setting this value to false is interpreted as ending editing mode (and NOT canceling it).
+		///     If it is desired to programmatically cancel editing, then <see cref="CancelEditing" />
+		///     shall be called.
+		/// </remarks>
+		public bool IsEditing
+		{
+			get { return (bool) GetValue(IsEditingProperty); }
+			set { SetValue(IsEditingProperty, value); }
+		}
+
+		/// <summary>
+		///     The text being displayed by this control.
+		///     The user may change this text, however the newly entered value is only
+		///     forwarded to this property when editing has finished, i.e. when:
+		///     - The user pressed enter
+		///     - The user focused another element
+		///     If the user pressed escape, then editing will be cancelled and the <see cref="Text" />
+		///     value will remain unchanged.
+		/// </summary>
+		public string Text
+		{
+			get { return (string) GetValue(TextProperty); }
+			set { SetValue(TextProperty, value); }
+		}
+
+		private static void OnIsEditingChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+		{
+			((EditableTextBlock) dependencyObject).OnIsEditingChanged((bool) args.NewValue);
+		}
+
+		private void OnIsEditingChanged(bool isEditing)
+		{
+			if (isEditing)
+			{
+				if (TextBox != null)
+					TextBox.Text = Text;
+
+				Dispatcher.BeginInvoke(DispatcherPriority.Background,
+					new Action(() =>
+					{
+						if (TextBox != null)
+						{
+							TextBox.Focus();
+							var text = TextBox.Text;
+							if (text != null)
+								TextBox.Select(0, text.Length);
+						}
+					}));
+			}
+			else
+			{
+				Text = TextBox?.Text;
+			}
+		}
+
+		/// <inheritdoc />
+		public override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			if (TextBlock != null)
+				TextBlock.MouseLeftButtonDown -= TextBlockOnMouseLeftButtonDown;
+
+			if (TextBox != null)
+			{
+				TextBox.LostFocus -= TextBoxOnLostFocus;
+				TextBox.KeyDown -= TextBoxOnKeyDown;
+			}
+
+			TextBlock = (TextBlock) GetTemplateChild(PART_TextBlock);
+			if (TextBlock != null)
+				TextBlock.MouseLeftButtonDown += TextBlockOnMouseLeftButtonDown;
+
+			TextBox = (TextBox) GetTemplateChild(PART_TextBox);
+			if (TextBox != null)
+			{
+				TextBox.LostFocus += TextBoxOnLostFocus;
+				TextBox.KeyDown += TextBoxOnKeyDown;
+			}
+		}
+
+		private void TextBlockOnMouseLeftButtonDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+		{
+			if (mouseButtonEventArgs.ClickCount >= 2)
+				IsEditing = true;
+		}
+
+		private void TextBoxOnKeyDown(object sender, KeyEventArgs keyEventArgs)
+		{
+			switch (keyEventArgs.Key)
+			{
+				case Key.Escape:
+					CancelEditing();
+					break;
+
+				case Key.Enter:
+					IsEditing = false;
+					break;
+			}
+		}
+
+		private void TextBoxOnLostFocus(object sender, RoutedEventArgs routedEventArgs)
+		{
+			CancelEditing();
+		}
+
+		/// <summary>
+		///     Hides the <see cref="TextBox" />, undoes all changes (if any) that were performed
+		///     since editing was enabled and displays the <see cref="TextBlock" /> again.
+		/// </summary>
+		public void CancelEditing()
+		{
+			var textBox = TextBox;
+			if (textBox != null)
+				textBox.Text = Text;
+
+			IsEditing = false;
+		}
+	}
+}
