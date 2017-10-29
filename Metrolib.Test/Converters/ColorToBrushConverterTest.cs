9f56753f4c2e539641490ee9ef2c130d28f0589d
Kittyfisto <kittyfisto87@gmail.com>
2017-10-29 22:27:16 +0100

    Introduced ColorToBrushConverter

---
+++ Metrolib.Test/Converters/ColorToBrushConverterTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Media;
+using FluentAssertions;
+using Metrolib.Converters;
+using NUnit.Framework;
+
+namespace Metrolib.Test.Converters
+{
+	[TestFixture]
+	public sealed class ColorToBrushConverterTest
+	{
+		[Test]
+		public void TestConvert1()
+		{
+			var converter = new ColorToBrushConverter();
+			var value = converter.Convert(Colors.Red, typeof(Brush), null, null);
+			value.Should().NotBeNull();
+			value.Should().BeOfType<SolidColorBrush>();
+			var brush = (SolidColorBrush) value;
+			brush.Color.Should().Be(Colors.Red);
+		}
+
+		[Test]
+		public void TestConvert2()
+		{
+			var converter = new ColorToBrushConverter();
+			converter.Convert(null, typeof(Brush), null, null).Should().BeNull();
+		}
+	}
+}
---
+++ Metrolib/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Metrolib.Converters
+{
+	/// <summary>
+	///     Converts <see cref="Color" /> values to <see cref="SolidColorBrush" />.
+	/// </summary>
+	public sealed class ColorToBrushConverter
+		: IValueConverter
+	{
+		/// <inheritdoc />
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is Color))
+				return null;
+
+			var color = (Color) value;
+			var brush = new SolidColorBrush(color);
+			brush.Freeze();
+			return brush;
+		}
+
+		/// <inheritdoc />
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
