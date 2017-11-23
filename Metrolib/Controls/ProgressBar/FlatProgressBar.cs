04ea9dbb6eec66ae9ace75a83883d12b00f7b080
Kittyfisto <kittyfisto87@gmail.com>
2017-11-23 19:36:50 +0100

    Fixed FlatProgressBar indeterminate display
    Fixed bug which caused the progress bar to not show a running bar when IsIndeterminate was set to true...

--- Metrolib/Controls/ProgressBar/FlatProgressBar.cs
+++ Metrolib/Controls/ProgressBar/FlatProgressBar.cs
@@ -12,9 +12,9 @@ namespace Metrolib
 	///     A progress bar in a "flat" style.
 	///     Displays the relative value as 'X%' in the progress bar.
 	/// </summary>
-	[TemplatePart(Name = "PART_Progress", Type = typeof (FrameworkElement))]
-	[TemplatePart(Name = "PART_ValuePositive", Type = typeof (FrameworkElement))]
-	[TemplatePart(Name = "PART_ValueNegative", Type = typeof (FrameworkElement))]
+	[TemplatePart(Name = "PART_Progress", Type = typeof(FrameworkElement))]
+	[TemplatePart(Name = "PART_ValuePositive", Type = typeof(FrameworkElement))]
+	[TemplatePart(Name = "PART_ValueNegative", Type = typeof(FrameworkElement))]
 	public class FlatProgressBar
 		: AbstractProgressBar
 	{
@@ -22,8 +22,8 @@ namespace Metrolib
 		///     Definition of the <see cref="IndeterminateValue" /> dependency property.
 		/// </summary>
 		public static readonly DependencyProperty IndeterminateValueProperty =
-			DependencyProperty.Register("IndeterminateValue", typeof (double), typeof (FlatProgressBar),
-			                            new PropertyMetadata(default(double)));
+			DependencyProperty.Register("IndeterminateValue", typeof(double), typeof(FlatProgressBar),
+			                            new PropertyMetadata(default(double), OnIndeterminateValueChanged));
 
 		private FrameworkElement _progress;
 		private FrameworkElement _valueNegative;
@@ -31,8 +31,8 @@ namespace Metrolib
 
 		static FlatProgressBar()
 		{
-			DefaultStyleKeyProperty.OverrideMetadata(typeof (FlatProgressBar),
-			                                         new FrameworkPropertyMetadata(typeof (FlatProgressBar)));
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(FlatProgressBar),
+			                                         new FrameworkPropertyMetadata(typeof(FlatProgressBar)));
 		}
 
 		/// <summary>
@@ -54,6 +54,17 @@ namespace Metrolib
 			set { SetValue(IndeterminateValueProperty, value); }
 		}
 
+		private static void OnIndeterminateValueChanged(DependencyObject dependencyObject,
+		                                                DependencyPropertyChangedEventArgs args)
+		{
+			((FlatProgressBar) dependencyObject).OnIndeterminateValueChanged();
+		}
+
+		private void OnIndeterminateValueChanged()
+		{
+			UpdateClip();
+		}
+
 		private void OnSizeChanged(object sender, SizeChangedEventArgs sizeChangedEventArgs)
 		{
 			UpdateClip();
@@ -68,35 +79,34 @@ namespace Metrolib
 		{
 			if (_progress != null && _valuePositive != null && _valueNegative != null)
 			{
-				double width = ActualWidth;
-				double height = ActualHeight;
+				var width = ActualWidth;
+				var height = ActualHeight;
 
 				if (IsIndeterminate)
 				{
-					double relativeValue = IndeterminateValue;
-					double knobwidth = width * 0.2;
-					double relativeValueWidth = relativeValue*(width + knobwidth) - knobwidth;
+					var relativeValue = IndeterminateValue;
+					var knobwidth = width * 0.2;
+					var relativeValueWidth = relativeValue * (width + knobwidth) - knobwidth;
 
 					var clip = CreateGeometry(relativeValueWidth,
 					                          relativeValueWidth + knobwidth,
-					                          0, height);
+					                          top: 0, bottom: height);
 
 					_progress.Clip = clip;
 				}
 				else
 				{
-					double relativeValue = (Value - Minimum) / (Maximum - Minimum);
-					double relativeValueWidth = relativeValue * width;
+					var relativeValue = (Value - Minimum) / (Maximum - Minimum);
+					var relativeValueWidth = relativeValue * width;
 
-					var clipLeft = CreateGeometry(0, relativeValueWidth, 0, height);
-					var clipRight = CreateGeometry(relativeValueWidth, width, 0, height);
+					var clipLeft = CreateGeometry(left: 0, right: relativeValueWidth, top: 0, bottom: height);
+					var clipRight = CreateGeometry(relativeValueWidth, width, top: 0, bottom: height);
 
 					_progress.Clip = clipLeft;
 					_valuePositive.Clip = clipLeft;
 
 					_valueNegative.Clip = clipRight;
 				}
-
 			}
 		}
 
@@ -104,13 +114,13 @@ namespace Metrolib
 		private static StreamGeometry CreateGeometry(double left, double right, double top, double bottom)
 		{
 			var geometry = new StreamGeometry();
-			using (StreamGeometryContext dc = geometry.Open())
+			using (var dc = geometry.Open())
 			{
-				dc.BeginFigure(new Point(left, top), true, true);
-				dc.LineTo(new Point(right, top), false, false);
-				dc.LineTo(new Point(right, bottom), false, false);
-				dc.LineTo(new Point(left, bottom), false, false);
-				dc.LineTo(new Point(left, top), false, false);
+				dc.BeginFigure(new Point(left, top), isFilled: true, isClosed: true);
+				dc.LineTo(new Point(right, top), isStroked: false, isSmoothJoin: false);
+				dc.LineTo(new Point(right, bottom), isStroked: false, isSmoothJoin: false);
+				dc.LineTo(new Point(left, bottom), isStroked: false, isSmoothJoin: false);
+				dc.LineTo(new Point(left, top), isStroked: false, isSmoothJoin: false);
 			}
 			return geometry;
 		}
