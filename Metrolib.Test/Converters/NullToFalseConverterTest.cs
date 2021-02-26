57c15f774277a44dd933759bc3469506c3586249
Kittyfisto <kittyfisto87@gmail.com>
2021-02-26 16:36:18 +0100

    Added NullToFalseConverterTest

---
+++ Metrolib.Test/Converters/NullToFalseConverterTest.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Metrolib.Converters;
+using NUnit.Framework;
+
+namespace Metrolib.Test.Converters
+{
+	[TestFixture]
+	public sealed class NullToFalseConverterTest
+	{
+		[Test]
+		public void TestConvert()
+		{
+			var converter = new NullToFalseConverter();
+			converter.Convert(null, null, null, null).Should().Be(false);
+			converter.Convert(true, null, null, null).Should().Be(true);
+			converter.Convert(42, null, null, null).Should().Be(true);
+			converter.Convert(false, null, null, null).Should().Be(true);
+		}
+	}
+}
---
+++ Metrolib/Converters/NullToFalseConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Metrolib.Converters
+{
+	/// <summary>
+	///     Converts null to false and everything else to true.
+	/// </summary>
+	public sealed class NullToFalseConverter
+		: IValueConverter
+	{
+		#region Implementation of IValueConverter
+
+		/// <inheritdoc />
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null)
+				return false;
+
+			return true;
+		}
+
+		/// <inheritdoc />
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
