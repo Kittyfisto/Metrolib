4d4043bbf38123f2e203a8a74e6b57122a5f2ff0
Simon Mießler <simon.miessler@gmx.net>
2021-11-13 19:08:58 +0100

    Added missing attributes once more

--- src/Metrolib/AssemblyInfo.cs
+++ src/Metrolib/AssemblyInfo.cs
@@ -3,6 +3,15 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
+
 #if DEBUG
 [assembly: InternalsVisibleTo("Metrolib.Test")]
 #else
